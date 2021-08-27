55ad81f3f8559e6fefa5cf1053d81974e2f68ab9
Vaibhav Shukla <shuklavaibhav336@gmail.com>
2021-08-27 22:30:18 +0530

    updated home views

--- home/views.py
+++ home/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 
-from article.models import Article
+from article.models import Article, Tag
 
 # Create your views here.
 
 def index(request):
-    featured_articles = Article.objects.filter(tag='featured')
-    articles = Article.objects.all()
-    return render(request, 'home/index.html')
+    # Selecting articles with featured tag
+    featured_tag = Tag.objects.get(name='featured')
+    featured_articles = Article.objects.filter(tags=featured_tag)[:3]
+
+    articles = Article.objects.all()[:12]
+    context = {
+        'featured_articles': featured_articles,
+        'articles': articles,
+    }
+    return render(request, 'home/index.html', context)
