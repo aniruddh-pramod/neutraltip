4bbb9a541fb682d43df2f7973e6f9b832feb6a44
Vaibhav Shukla <shuklavaibhav336@gmail.com>
2021-08-28 15:23:26 +0530

    updated urls

--- article/urls.py
+++ article/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('article/<slug:slug>/', views.article_detail, name='article_detail'),
     path('category/', views.category_list, name='category_list'),
     path('category/<str:category>/', views.category, name='category_page'),
+    path('tag/<str:tag>/', views.tag, name='tag_page'),
     path('blog/', views.blog, name='blog_index'),
     path('blog/<slug:blog>/', views.blog_detail, name='blog_detail'),
     path('about/', views.about, name='about_page'),
