ee03643bd4d9197203489b25c1a0eba924205d25
Vaibhav Shukla <shuklavaibhav336@gmail.com>
2021-08-28 15:23:19 +0530

    updated views

--- article/views.py
+++ article/views.py
@@ -4,10 +4,27 @@ from django.shortcuts import render
 from .models import Article
 
 def article_detail(request, slug):
+    article = Article.objects.get(slug=slug)
+    article_id = article.id
+
+    # Previous article
+    if article_id > 1:
+        previous_article = Article.objects.get(id=article_id - 1)
+    else:
+        previous_article = Article.objects.get(id=1)
+    
+    # Next article
+    if article_id < Article.objects.count():
+        next_article = Article.objects.get(id=article_id + 1)
+    else:
+        next_article = Article.objects.get(id=Article.objects.count())
+
     context = {
-        'article': Article.objects.get(slug=slug)
+        'article': article,
+        'previous_article': previous_article,
+        'next_article': next_article,
     }
-    return render(request, 'article/article-detail.html', context)
+    return render(request, 'article/single-standard.html', context)
 
 
 def blog(request):
@@ -17,6 +34,8 @@ def blog_detail(request, blog):
 def category(request, category):
     return render(request, 'article/article-detail.html')
 def category_list(request):
+    return render(request, 'article/article-detail.html')
+def tag(request, tag):
     return render(request, 'article/article-detail.html')
 def about(request):
     return render(request, 'article/article-detail.html')
