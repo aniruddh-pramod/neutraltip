7c80e94c5507117ec0b37c74276d7ea2d4927c4c
Vaibhav Shukla <shuklavaibhav336@gmail.com>
2021-08-29 15:09:12 +0530

    updated ACCOUNT views

--- account/views.py
+++ account/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from django.contrib.auth.models import User
+# from django.contrib.auth.decorators import login_required
+
+from article.models import Article
 
 # Create your views here.
+
+def user_profile(request, user):
+    user = get_object_or_404(User, username=user)
+
+    tab = request.GET.get('tab')
+    print(tab)
+
+    context = {
+        'tab': tab,
+        'user': user,
+    }
+
+    if tab == 'articles':
+        articles = Article.objects.filter(author=user)
+        context['articles'] = articles
+    return render(request, 'account/user-profile.html', context)
