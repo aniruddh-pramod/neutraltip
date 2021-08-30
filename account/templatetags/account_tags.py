6a74fecd4bb73e2e201685add6588c5c748ddc3e
Vaibhav Shukla <shuklavaibhav336@gmail.com>
2021-08-30 21:22:50 +0530

    added custom templatetags in ARTICLE

---
+++ account/templatetags/account_tags.py
@@ -0,0 +1,44 @@
+from django import template
+from django.utils.safestring import mark_safe
+
+from django.urls import reverse
+from datetime import datetime
+
+register = template.Library()
+
+
+@register.simple_tag
+def profile_pic(user):
+    profile = user.userprofile
+
+    if not profile.display_profile:
+        return ''
+    
+    if profile.dob:
+        profile_dob = profile.dob.strftime('%d/%m/%Y')
+    else:
+        profile_dob = ''
+
+    if profile.profile_pic:
+        return mark_safe(f'<img class="profile-pic" src="{profile.profile_pic.url}" alt="{profile.get_full_name()}" />')
+    else:
+        return mark_safe(f'<svg data-jdenticon-value="{profile.get_full_name()} {profile_dob}"></svg>')
+
+
+@register.simple_tag
+def user_name(user, href=False):
+    profile = user.userprofile
+    
+    if not profile.display_profile:
+        return ''
+    
+    if profile.get_full_name:
+        if href:
+            return mark_safe(f'<a href="{reverse("account:user_profile", kwargs={"user": user.username})}">{profile.get_full_name()}</a>')
+        else:
+            return mark_safe(f'{profile.get_full_name()}')
+    else:
+        if href:
+            return mark_safe(f'<a href="{reverse("account:user_profile", kwargs={"user": user.username})}">{user.username}</a>')
+        else:
+            return mark_safe(f'{user.username}')
