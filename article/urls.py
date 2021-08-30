af9bba73353fbe99fe9c4c7fffb4630897c314e7
Vaibhav Shukla <shuklavaibhav336@gmail.com>
2021-08-30 21:24:38 +0530

    updated ARTICLE urls

--- article/urls.py
+++ article/urls.py
@@ -2,13 +2,13 @@ from django.urls import path
 
 from . import views
 
+app_name = 'article'
 urlpatterns = [
-    path('article/<slug:slug>/', views.article_detail, name='article_detail'),
+    path('article/<slug:slug>/', views.ArticleDetailView.as_view(), name='detail'),
+    path('search/<str:query>/', views.SearchArticlesListView.as_view(), name='search'),
     path('category/', views.category_list, name='category_list'),
     path('category/<str:category>/', views.category, name='category_page'),
     path('tag/<str:tag>/', views.tag, name='tag_page'),
     path('blog/', views.blog, name='blog_index'),
     path('blog/<slug:blog>/', views.blog_detail, name='blog_detail'),
-    path('about/', views.about, name='about_page'),
-    path('contact/', views.contact, name='contact_page'),
 ]
