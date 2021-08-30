ce24ba7ac1dda2d4afc95e4eba0d8bb7de523839
Vaibhav Shukla <shuklavaibhav336@gmail.com>
2021-08-30 21:19:07 +0530

    updated HOMW urls

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 
 from . import views
 
+app_name = 'home'
 urlpatterns = [
-    path('', views.index, name='home_index'),
+    path('', views.ArticleListView.as_view(), name='index'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('support/', views.support, name='support'),
+    path('credits/', views.credits, name='credits'),
+    path('faq/', views.faq, name='faq'),
 ]
