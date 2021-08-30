b19203454a172ea97cd3f1db6d847ca990b8f6f0
Vaibhav Shukla <shuklavaibhav336@gmail.com>
2021-08-30 21:19:17 +0530

    updated HOME views (class based view)

--- home/views.py
+++ home/views.py
@@ -1,4 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import ListView
 
 from article.models import Article, Tag
 
@@ -6,37 +8,35 @@ import math
 
 # Create your views here.
 
-def validate_page_number(number, paginations):
-    # if number is positive integer
-    number = int(number) if (number.isdigit() and int(number) > 0) else 1
-    # if number is in pagination range
-    number = number if number <= paginations else paginations
-    
-    return number
+class ArticleListView(ListView):
+    model = Article
+    template_name = 'home/index.html'
+    context_object_name = 'articles'
+    paginate_by = 10
+
+    def get_queryset(self):
+        # Call the base implementation first to get a context
+        articles = super(ArticleListView, self).get_queryset()
+        # Add extras
+        articles = articles[:108]
+        return articles
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super(ArticleListView, self).get_context_data(**kwargs)
+        # Add extras
+        context['featured_articles'] = Article.objects.filter(tags__name__in=['featured'])[:3]
+
+        return context
 
 
-def index(request):
-    # Selecting articles with featured tag
-    featured_tag = Tag.objects.get(name='featured')
-    featured_articles = Article.objects.filter(tags=featured_tag)[:3]
-
-    # Pagination
-    paginations = math.ceil(Article.objects.all().count() / 2)
-
-    page_no = request.GET.get('page', '1')
-    page_no = validate_page_number(page_no, paginations)
-
-    pagination_start = (page_no - 1) * 2
-    pagination_end = pagination_start + 2
-
-    relative_paginations = paginations + (page_no - 1) if paginations > 9 else paginations
-
-    articles = Article.objects.all()[pagination_start:pagination_end]
-
-    context = {
-        'featured_articles': featured_articles,
-        'articles': articles,
-        'paginations': range(1, relative_paginations + 1),
-        'current_page': page_no,
-    }
-    return render(request, 'home/index.html', context)
+def about(request):
+    return HttpResponse('about')
+def contact(request):
+    return HttpResponse('contact')
+def support(request):
+    return HttpResponse('support')
+def credits(request):
+    return HttpResponse('credits')
+def faq(request):
+    return HttpResponse('faq')
