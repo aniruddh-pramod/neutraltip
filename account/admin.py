d1ae00cbbe9c6d0345cc9cd1d8f916b3bf2462dc
Vaibhav Shukla <shuklavaibhav336@gmail.com>
2021-09-02 17:07:23 +0530

    indexed models in admin site

--- account/admin.py
+++ account/admin.py
@@ -6,4 +6,4 @@ from .models import UserProfile
 
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('user', 'profile_pic', 'website', 'display_profile')
