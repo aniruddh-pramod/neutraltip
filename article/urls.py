88aff46daab7a717551e4b183c5cecd70aca57af
Vaibhav Shukla <shuklavaibhav336@gmail.com>
2021-09-02 17:04:51 +0530

    updated ARTICLE urls

--- article/urls.py
+++ article/urls.py
@@ -6,9 +6,6 @@ app_name = 'article'
 urlpatterns = [
     path('article/<slug:slug>/', views.ArticleDetailView.as_view(), name='detail'),
     path('search/<str:query>/', views.SearchArticlesListView.as_view(), name='search'),
-    path('category/', views.category_list, name='category_list'),
-    path('category/<str:category>/', views.category, name='category_page'),
-    path('tag/<str:tag>/', views.tag, name='tag_page'),
     path('blog/', views.blog, name='blog_index'),
     path('blog/<slug:blog>/', views.blog_detail, name='blog_detail'),
 ]
