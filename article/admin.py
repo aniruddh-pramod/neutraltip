39beeb12870cad18be0e3d2ba3e588b1bd1291e5
Vaibhav Shukla <shuklavaibhav336@gmail.com>
2021-09-05 15:12:51 +0530

    updated ARTICLE admin oage

--- article/admin.py
+++ article/admin.py
@@ -1,16 +1,21 @@
 from django.contrib import admin
 
-from .models import Article, Comment, Tag, Category
+from .models import Article, Comment, Tag, Category, ArticleSubmission
 
 # Register your models here.
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('title', 'author', 'category', 'date', 'time', 'updated_at', 'views')
+    list_filter = ('category', 'date', 'updated_at')
+    search_fields = ('title', 'body', 'excerpt',)
+
+    def views(self, obj):
+        return obj.hit_count.hits
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name', 'email', 'article', 'datetime')
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
@@ -18,4 +23,8 @@ class TagAdmin(admin.ModelAdmin):
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(ArticleSubmission)
+class ArticleSubmissionAdmin(admin.ModelAdmin):
     pass
