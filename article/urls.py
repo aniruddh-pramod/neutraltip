331ed32884b3c5d828410bf6efcf6896ac0853d6
Vaibhav Shukla <shuklavaibhav336@gmail.com>
2021-09-05 15:11:30 +0530

    updated ARTICLE urls

--- article/urls.py
+++ article/urls.py
@@ -5,6 +5,8 @@ from . import views
 app_name = 'article'
 urlpatterns = [
     path('article/<slug:slug>/', views.ArticleDetailView.as_view(), name='detail'),
+    path('submit/', views.ArticleSubmissionView.as_view(), name='submit'),
+    path('submit/success/', views.article_submission_success, name='submit-success'),
     path('search/<str:query>/', views.SearchArticlesListView.as_view(), name='search'),
     path('blog/', views.blog, name='blog_index'),
     path('blog/<slug:blog>/', views.blog_detail, name='blog_detail'),
