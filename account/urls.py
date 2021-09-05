111baf755a21ed6d416972bb066b41be25082607
Vaibhav Shukla <shuklavaibhav336@gmail.com>
2021-09-05 16:56:06 +0530

    updated ACCOUNT urls

--- account/urls.py
+++ account/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 app_name = 'account'
 urlpatterns = [
+    path('logout/', views.LogoutView.as_view(), name='logout'),
     path('<str:user>/', views.user_profile, name='user_profile'),
+    path('<str:user>/edit', views.UserProfileEditView.as_view(), name='user_profile_edit'),
 ]
