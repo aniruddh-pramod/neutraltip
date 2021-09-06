bc99ead649bd9754cdcd073b52e2df2974ec2195
Vaibhav Shukla <shuklavaibhav336@gmail.com>
2021-09-07 00:11:08 +0530

    initial commit

--- account/forms.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm
-
-from .models import UserProfile
-
-class UserProfileEditForm(ModelForm):
-    class Meta:
-        model = UserProfile
-        fields = '__all__'
-    
--- home/context_processors.py
+++
@@ -1,4 +0,0 @@
-from article.models import Category
-
-def categories(request):
-    return {'categories': Category.objects.all()[::-1]}
