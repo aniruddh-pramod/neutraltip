a8488c3797acb1f55c210669b8b7a68b0eee2064
Vaibhav Shukla <shuklavaibhav336@gmail.com>
2021-09-07 16:01:35 +0530

    updated ACCOUNT models

--- account/models.py
+++ account/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-from neutraltip.settings import BASE_DIR
-
 # Signal to create a user profile when a user is created
 from django.dispatch import receiver
 from django.db.models.signals import post_save
