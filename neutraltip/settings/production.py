75c3847e659ad59d136f36899dd1cc76edb6052c
Vaibhav Shukla <shuklavaibhav336@gmail.com>
2021-09-07 16:39:15 +0530

    manually reverting last commit and commenting whitenoise gzip

--- neutraltip/settings/production.py
+++ neutraltip/settings/production.py
@@ -10,8 +10,7 @@ SECRET_KEY = env('SECRET_KEY')
 
 DEBUG = env.bool('DEBUG', default=False)
 
-# ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'neutraltip.herokuapp.com']
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
@@ -22,7 +21,7 @@ DATABASES['default']['CONN_MAX_AGE'] = 60
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Whitenoise gzip compression
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
