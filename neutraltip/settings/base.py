7d1272b47ac3c7cb1d74c4f2bbe22bc7a73c7d3e
Vaibhav Shukla <shuklavaibhav336@gmail.com>
2021-09-12 17:41:44 +0530

    tried django-cloudinary-storage

--- neutraltip/settings/base.py
+++ neutraltip/settings/base.py
@@ -157,11 +157,6 @@ STATICFILES_DIRS = [
 ]
 
 
-# Media settings
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
-
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
